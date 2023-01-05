b29f613cb9c3b7f1542d1894107a83e569f97905
David Walker <thewalker.david@gmail.com>
2023-01-05 16:03:08 -0500

    Added name to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_204608) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_210239) do
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.integer "artist_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_204608) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.integer "user_id"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
