b36d51fa4b13a14596897b7af40b8d171079df9f
David Walker <thewalker.david@gmail.com>
2023-01-05 16:22:54 -0500

    Added record association

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,5 @@
 class Song < ApplicationRecord
+    belongs_to :artist
+    has_many :playlists
+    has_many :playlists_tracks, thorught: :playlists, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :playlists, dependent: :destory
+  has_many :playlists_tracks, thorugh: :playlists, source: :song
 end
