f626519bee765b31bfd3d07f2ab851300172f90c
David Walker <thewalker.david@gmail.com>
2023-01-11 15:03:45 -0500

    Added a form to add song to playlist

--- app/models/playlist_track.rb
+++ app/models/playlist_track.rb
@@ -1,5 +1,7 @@
 class PlaylistTrack < ApplicationRecord
-    
+    validates :song_id, presence: true
+    validates :playlist_id, presence: true
+
     belongs_to :song
     belongs_to :playlist
 end
