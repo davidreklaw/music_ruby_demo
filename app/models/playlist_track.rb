36dba4cd021c4c1d16e9a24db192129888a87298
David Walker <thewalker.david@gmail.com>
2023-01-27 11:35:31 -0500

    Changed model for playlist track

--- app/models/playlist_track.rb
+++ app/models/playlist_track.rb
@@ -1,6 +1,6 @@
 class PlaylistTrack < ApplicationRecord
-    validates :song_id, presence: true
-    validates :playlist_id, presence: true
+    validates :song_id_id, presence: true
+    validates :playlist_id_id, presence: true
 
     belongs_to :song
     belongs_to :playlist
